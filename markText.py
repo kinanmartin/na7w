33810850fdc4ab08c049a14a9839178e15fc2f37
Kinan Martin <kinanmartin0@gmail.com>
2023-11-26 12:20:54 -0500

    reorder algo

--- markText.py
+++ markText.py
@@ -11,11 +11,11 @@ def mark_pos(text):
             if len(token.words) < 1 and token.words[0].lemma == "لِ" and token.words[1].text[:2] == "ال" and 'Definite=Def' in token.words[1].feats:
                 # Strip the inital alef
                 token.words[1].text = token.words[1].text[1:]
-            # If this token is a multi-word token, there will be multiple "words" in this list
-            tagged.append([(word.text, word.xpos) for word in token.words])
             # Add space to end of token's text only if required:
             if i != len(sent.tokens) - 1 and token.end_char != sent.tokens[i + 1].start_char:
-                tagged[-1][-1] = (tagged[-1][-1][0] + ' ', tagged[-1][-1][1])
+                token.words[-1].text = token.words[-1].text + ' '
+            # If this token is a multi-word token, there will be multiple "words" in this list
+            tagged.append([(word.text, word.xpos) for word in token.words])
 
     return tagged
 
